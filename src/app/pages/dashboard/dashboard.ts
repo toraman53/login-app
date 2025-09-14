154185c3aa1a8e754059eb01daa23db5788c474b
ishak <toraman53@gmail.com>
2025-09-14 14:37:39 +0300

    authLoginLogout

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.css'
 })
 export class Dashboard {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();          // oturumu kapat
+    this.router.navigate(['/login']);   // login sayfasına dön
+  }
 
 }
